700251b6d26e59016a691b389895650084ef6a2b
alexbotha <alexbotha@hotmail.co.uk>
2022-07-19 11:55:20 +0100

    commit tree 9ad7ce730090fb5e6882cd70aa0d478168f71c54

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(element){
+
+  let winYear = element.find(y => y.result === 'W')
+  
+  if (winYear === undefined){
+    console.log(winYear)
+    return winYear
+  } else {
+    console.log(winYear.year)
+    return winYear.year;
+  }
+}
